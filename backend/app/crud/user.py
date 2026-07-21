aa0b5a6abdd97230e586fcf892516919cee6f997
Taha N. <tahanoursalehi@gmail.com>
2026-07-21 22:48:05 +0330

    feat: implement password hashing

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -3,12 +3,13 @@ from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.user import UserCreate
 
+from app.security import hash_password
 
 def create_user(db: Session, user_data: UserCreate):
     user = User(
         username=user_data.username,
         display_name=user_data.display_name,
-        password_hash=user_data.password,
+        password_hash=hash_password(user_data.password),
         birth_year=user_data.birth_year,
         role=user_data.role,
     )
