e088d8c8cbb779614da84b15d6a2c7fc4f69ac15
Taha N. <tahanoursalehi@gmail.com>
2026-07-21 19:11:40 +0330

    feat: create database tables

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
+from app.database import Base, engine
+import app.models
+
 app = FastAPI(
     title="TAAB API",
     version="0.1.0"
 )
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
